547d926fc8bcdcccf92c2043ea461400f98e760b
Andres Chavez <t4t4x777@gmail.com>
2026-09-12 14:09:25 -0400

    feat: initialize the spring boot project

---
+++ moviesSpringBoot/src/main/java/com/wayneEntertainment/moviesSpringBoot/MoviesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wayneEntertainment.moviesSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesSpringBootApplication.class, args);
+	}
+
+}
---
+++ moviesSpringBoot/src/test/java/com/wayneEntertainment/moviesSpringBoot/MoviesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayneEntertainment.moviesSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
